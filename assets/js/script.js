3bc5b47cab4e0820465568dbb3e795c179ac1891
omid <omedmm3@gmail.com>
2024-05-13 13:45:38 +0430

    changed typing speed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var typed = new Typed('#typing', {
     strings: ["Hello Wellcome to mi Website", 'I am Omid Azadi a funny Developer from Afghanistan', 'a linux lower', 'currently learning golang and bloakchain', 'I Love Taekwondo and Kung Fu', 'سینه خواهم شرحه شرحه از فراق تا بگویم شرح درد اشتیاق'],
-    typeSpeed: 100,
+    typeSpeed: 70,
     backSpeed: 70,
     startDelay: 500,
     showCursor: true,
